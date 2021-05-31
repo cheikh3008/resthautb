8eea2f2a2d65eb49c5b2a415687a7e6d5ec6ea88
cheikh3008 <cheikh3008@gmail.com>
2021-05-31 03:57:31 +0000

    remove add panier ang get panier in the controller reservation

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -12,43 +12,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CommandeController extends AbstractController
 {
     /**
-     * @Route("/api/panier/{id}", name="add_panier")
+     * @Route("/api/commande", name="add_commande")
      */
-    public function index($id , SessionInterface $session, SerializerInterface $serializer): Response
+    public function index(): Response
     {
-        $panier = $session->get('panier', []) ; 
-        if (!empty($panier[$id])) {
-            $panier[$id]++;
-        } else {
-            $panier[$id] = 1;
-        }
-        $session->set('panier', $panier);
-        $data = $serializer->serialize($panier, 'json');
-        return new Response($data, 200, [
+        return new Response('test ok', 200, [
             'Content-Type' => 'application/json'
         ]);
     }
 
-    /**
-     * @Route("/api/paniers", name="list_panier")
-     */
-    public function list (SessionInterface $session, SerializerInterface $serializer, PlatRepository $platRepository)
-    {
-        $panier = $session->get('panier', []) ; 
-        $dataPanier = [];
-        foreach ($panier as $id => $quantite) {
-            $dataPanier [] = [
-                'plat' => $platRepository->find($id),
-                'quantite' => $quantite,
-            ];
-        }
-        
-        foreach ($dataPanier as $value) {
-            $value['plat']->setImage((base64_encode(stream_get_contents($value['plat']->getImage()))));
-        }
-        $data = $serializer->serialize($dataPanier, 'json');
-        return new Response($data, 200, [
-            'Content-Type' => 'application/json'
-        ]);
-    }
 }
